caced0f30718431fa03f991e38c43cb2f149291d
static-tutorial-1700 <ihposey@crimson.ua.edu>
2020-10-31 05:00:41 +0000

    Automatically backed up by Learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -19,5 +19,5 @@ end
 def find_valid_calls(planeteer_calls)
   valid_calls = ["Earth!", "Wind!", "Fire!", "Water!", "Heart!"]
   array.find do |calls|
-    calls ==
+    calls == "Earth!" || "Wind"
 end
